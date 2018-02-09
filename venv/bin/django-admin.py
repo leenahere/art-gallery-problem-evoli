f63003caa9ad00fccd6437f304dc9b6e7ba95a0c
Lina Peters <lina.pete@googlemail.com>
2018-02-09 18:13:36 +0100

    Revert "Merge pull request #1 from fzsauer/master"
    This reverts commit 3f5a23abd4ef68b3d37c16215512bff04ba594de, reversing
    changes made to 0c5d8052ead5c8da108732bb45706d7f2bef1c60.

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/fabiansauer/PyCharmProjects/art-gallery-problem-evoli/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
